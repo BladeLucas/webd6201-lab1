883ef70ec0db1a4cdb65a60b88ef54bf0eb0b9a0
Blade Lucas <elffe2@hotmail.ca>
2023-02-10 20:04:48 -0500

    Started DOM in main.js

---
+++ scripts/app.js
@@ -0,0 +1,104 @@
+(function () {
+    //Declarations
+    let name1 = "Darren"
+    let name2 = "Blade"
+
+    //Home
+    let welcome = "Welcome"
+    let welcomeText = "Welcome to our site"
+
+    //Projects
+    let projects = "Projects"
+
+    let project1 = "erwerw"
+    let project2 = "twtrfhs"
+    let project3 = "fkhyd" 
+
+    //Services
+    let skills = "Skills"
+
+    let darrenSkill1 = "hbgs"
+    let darrenSkill2 = "wrgsd"
+    let darrenSkill3 = "luik"
+
+    let bladeSkill1 = "afthth"
+    let bladeSkill2 = "lhgbhc"
+    let bladeSkill3 = "zfsgre"
+
+    //About
+    let darrenDetails = "ogftgngbd"
+    let bladeDetails = "dqfvrh"
+    let resume = "Resumes"
+
+    //Text injections
+    //Home
+    function displayText() {
+        switch (document.title) {
+            case "Lab 1 - Home":
+                let mainContent = document.getElementsByTagName("main")[0]
+                mainContent.setAttribute("class", "container")
+
+                let welcomeMessage = document.createElement("h1")
+                welcomeMessage.setAttribute("id","WelcomeMessage")
+                welcomeMessage.setAttribute("class", "mt-3")
+                
+                welcomeMessage.textContent = welcomeText
+            
+                mainContent.appendChild(welcomeMessage)
+                break;
+            
+            case "Lab 1 - Products":
+                let mainProduct = document.getElementsByTagName("mainProduct")[0]
+                mainProduct.setAttribute("class", "container")
+
+                let productMessage = document.createElement("h1")
+                productMessage.setAttribute("id","ProductMessage")
+                productMessage.setAttribute("class", "mt-3")
+
+                let productString = `${ projects } ${ name1 }`
+            
+                productMessage.textContent = productString
+            
+                mainProduct.appendChild(productMessage)
+
+                break;
+                
+                
+        }
+
+    }
+    //Projects
+    function changeProductsLink() {
+        let test = document.getElementsByTagName("a")[0]
+        if(test = "Products") {
+            let txt = Element.textContent
+            console.log(txt)
+        }
+    }
+
+    //Skills
+
+    //Abour
+
+
+    //Add new nav bar link
+
+    //Add nav bar at the bottom, fixed + copywrite
+
+    function Start() {
+        console.log("App Started!")
+
+        // switch (document.title) {
+        //     case "Lab 1 - Home":
+        //         displayText()
+        //         break;
+        //     case "Lab 1 - Products":
+        //         displayText()
+        //         break;
+        // }
+        changeProductsLink()
+        displayText()
+    }
+
+    window.addEventListener("load", Start)
+})()
